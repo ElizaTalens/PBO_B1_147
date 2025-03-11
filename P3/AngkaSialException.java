4b529d0aabf9db845758447a5db050c2ca85f37c
ElizaTalens <elizasirait07@gmail.com>
2025-03-12 00:35:55 +0700

    Add files via upload

---
+++ P3/AngkaSialException.java
@@ -0,0 +1,11 @@
+/* Nama File : AngkaSialException.java
+ * Deskripsi : Berisi atribut dan method dalam class AngkaSialException
+ * Pembuat : Eliza Talent Sirait
+ * Tanggal : 10 Maret 2025
+ */
+
+public class AngkaSialException extends Exception{
+    public AngkaSialException(){
+        super("jangan memasukkan angka 13 karena angka sial");
+    }
+}
